3c710581bec97f16f735faa64b5339f0b0a376b3
mewsu <zeiontic@gmail.com>
2020-08-07 19:16:06 -0400

    integrate login route with api

--- src/App.js
+++ src/App.js
@@ -9,9 +9,29 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoggedIn: false
+      isLoggedIn: false,
+      userDetails: null
     };
   }
+
+  setUserDetails = data => {
+    console.log("setting user details: ", JSON.stringify(data));
+    const userDetails = {
+      firstName: data.first_name,
+      lastName: data.firstName,
+      email: data.email,
+      addressId: data.address_id,
+      role: data.role
+    };
+
+    if (data.role === "customer") {
+      userDetails.id = data.customer_id;
+    } else if (data.role === "staff") {
+      userDetails.id = data.staff_id;
+    }
+
+    this.setState({ userDetails, isLoggedIn: true });
+  };
 
   render() {
     return (
@@ -19,7 +39,7 @@ class App extends React.Component {
         <TopNav isLoggedIn={this.state.isLoggedIn} />
         <Router>
           <FilmPage path="/" />
-          <LoginPage path="login" />
+          <LoginPage path="login" setUserDetails={this.setUserDetails} />
         </Router>
       </div>
     );
@@ -29,6 +49,12 @@ class App extends React.Component {
 const TopNav = props => {
   return (
     <div id="top-nav">
+      <Link to="profile">
+        <button className="nav-button" disabled={!props.isLoggedIn}>
+          My Profile
+        </button>
+      </Link>
+
       <Link to="login">
         <button className="nav-button" disabled={props.isLoggedIn}>
           Login
--- src/LoginPage.js
+++ src/LoginPage.js
@@ -3,6 +3,7 @@ import { Link } from "@reach/router";
 export default class LoginPage extends React.Component {
   constructor(props) {
     super(props);
+    console.log(props);
     // console.log(sampleData);
     this.state = {
       emailInput: "",
@@ -26,12 +27,23 @@ export default class LoginPage extends React.Component {
     this.setState({ passwordPlaceholder: "", passwordInput: e.target.value });
   };
 
-  onLoginPressed = () => {
-    const username = this.state.emailInput;
-    const password = this.state.passwordInput;
-    console.log("logging in with creds: ", { username, password });
+  onLoginPressed = async () => {
+    // const username = this.state.emailInput;
+    // const password = this.state.passwordInput;
 
-    if (username == "") {
+    const testdata = {
+      email: "mary.smith@sakilacustomer.org",
+      password: "pass123",
+      role: "customer"
+    };
+
+    const email = testdata.email;
+    const password = testdata.password;
+    const role = testdata.role;
+
+    console.log("logging in with creds: ", { email, password, role });
+
+    if (email == "") {
       this.setState({ emailPlaceholder: "Please enter your email." });
     }
 
@@ -39,22 +51,31 @@ export default class LoginPage extends React.Component {
       this.setState({ passwordPlaceholder: "Please enter your password." });
     }
 
-    // TODO: not implemented on server yet
-    // fetch(`http://localhost:3001/login`, {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/x-www-form-urlencoded"
-    //   },
-    //   body: {
-    //     username,
-    //     password
-    //   }
-    // })
-    //   .then(res => res.json())
-    //   .then(json => {
-    //     console.log("response: ", json);
-    //     // ...
-    //   });
+    try {
+      const response = await fetch(`http://localhost:3001/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email,
+          password,
+          role
+        })
+      });
+
+      if (!response.ok) {
+        console.log("login failed");
+        return;
+      }
+      const data = await response.json();
+      console.log("login ok");
+      console.log(data);
+      this.props.setUserDetails(data);
+    } catch (err) {
+      console.log("login error: ", err);
+      return;
+    }
   };
 
   render() {
